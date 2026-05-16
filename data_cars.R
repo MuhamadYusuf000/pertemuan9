50637ef1483dfa0b4a1d75fe6dbedf8e43ded8bc
Yusuf <ymuhamad221@gmail.com>
2026-05-17 00:40:19 +0700

    Membuat Scatterplot dan garis regresi linearnya

--- data_cars.R
+++ data_cars.R
@@ -11,3 +11,17 @@ for (i in 1:n){
 }
 
 hist(mtcars$mpg)
+
+###### Try Tambah/edit Syntax #####
+# Membuat scatter plot hubungan speed dan distance
+plot(cars$speed, cars$dist,
+     main = "Hubungan antara Speed dan Stopping Distance",
+     xlab = "Speed",
+     ylab = "Stopping Distance",
+     col = "blue",
+     pch = 19)
+
+# Menambahkan garis regresi linear
+abline(lm(cars$dist ~ cars$speed),
+       col = "red",
+       lwd = 2)
